1a13e5cca52a0958d276caccb48e3195bcdf4b67
aiwkz <federicoj.pp@gmail.com>
2023-05-10 18:04:57 +0200

    ✨ Added Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import BrainIcon from './brain.png';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className='logo-container'>
+            <Tilt>
+                <img 
+                    alt="Brain icon" 
+                    src={BrainIcon} 
+                    className='tilt'
+                />
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
