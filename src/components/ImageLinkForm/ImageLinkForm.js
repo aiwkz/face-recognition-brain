7fca0a09eca59ff81835fe7f2aaf61d5e4d26976
aiwkz <federicoj.pp@gmail.com>
2023-05-10 18:05:15 +0200

    ✨ Added ImageLinkForm component

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+    return (
+        <div className='form-container'>
+            <div className='form-title'>
+                <span>This magic brain will detect faces in your images.</span>
+                <span>Paste the img url to try!</span>
+            </div>
+            <div className='center'>
+                <input type='text'/>
+                <button 
+                    className='form-button'
+                    style={{ '--clr':'#0FF0FC' }}
+                >
+                    <span>Detect</span>
+                    <i></i>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
