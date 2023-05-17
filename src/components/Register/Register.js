e3c03afa1b44de985513548192b4bdbead05d7ed
aiwkz <federicoj.pp@gmail.com>
2023-05-17 18:50:56 +0200

    💄 Fixes signin and resgister form UI

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,7 +3,7 @@ import React from "react";
 const Register = ({ onRouteChange }) => {
     return (
         <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
-            <main className='pa4 black-80'>
+            <main className='pa4 black-80 w-100'>
                 <div className='measure'>
                     <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
                         <legend className='f2 fw6 ph0 mh0'>
@@ -54,7 +54,7 @@ const Register = ({ onRouteChange }) => {
                     </fieldset>
                     <div>
                         <input 
-                            className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib' 
+                            className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w-100' 
                             type='submit' 
                             value='Register'
                             onClick={() => onRouteChange('home')}
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SignIn = ({ onRouteChange }) => {
     return (
         <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
-            <main className='pa4 black-80'>
+            <main className='pa4 black-80 w-100'>
                 <div className='measure'>
                     <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
                         <legend className='f2 fw6 ph0 mh0'>
@@ -40,7 +40,7 @@ const SignIn = ({ onRouteChange }) => {
                     </fieldset>
                     <div>
                         <input 
-                            className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib' 
+                            className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w-100' 
                             type='submit' 
                             value='Sign in'
                             onClick={() => onRouteChange('home')}
