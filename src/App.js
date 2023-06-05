a5b7ef15be8b1ee77c751d47ddeab1a67f9a0446
aiwkz <federicoj.pp@gmail.com>
2023-06-05 17:04:40 +0200

    🚧 rolls back body changes

--- src/App.js
+++ src/App.js
@@ -68,10 +68,12 @@ class App extends Component {
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input })
 
-    fetch('https://face-recognition-brain-api-gu0q.onrender.com/imageUrl', {
-        method: 'post',
-        headers: {'Content-type': 'application/json'},
-        body: this.state.input,
+    fetch('http://face-recognition-brain-api-gu0q.onrender.com/imageUrl', {
+      method: 'post',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input,
+      })
     })
       .then(response => response.json())
       .then(result => this.displayFaceBox(this.calculateFaceLocations(result)))
