a031ede0cd2abeebd2168d44b40aaab8774f1914
aiwkz <federicoj.pp@gmail.com>
2023-07-07 12:02:50 +0200

    ✨ adds spinner for signIn and register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { PacmanLoader } from 'react-spinners';
+
+import './Register.css';
 
 class Register extends Component {
     constructor(props) {
@@ -6,7 +9,8 @@ class Register extends Component {
         this.state = {
             name: '',
             email: '',
-            password: ''
+            password: '',
+            isLoading: false
         }
     }
 
@@ -23,9 +27,10 @@ class Register extends Component {
     }
 
     onSubmit = () => {
+        this.setState({ isLoading: true })
         fetch('https://face-recognition-brain-api-gu0q.onrender.com/register', {
             method: 'post',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 name: this.state.name,
                 email: this.state.email,
@@ -34,10 +39,11 @@ class Register extends Component {
         })
             .then(response => response.json())
             .then(user => {
-                if(user.id) {
+                if (user.id) {
                     this.props.loadUser(user);
                     this.props.onRouteChange('home')
-                }   
+                }
+                this.setState({ isLoading: false })
             })
             .catch(console.log)
     }
@@ -52,72 +58,77 @@ class Register extends Component {
 
     render() {
         return (
-            <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
-                <main className='pa4 black-80 w-100'>
-                    <div className='measure'>
-                        <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
-                            <legend className='f2 fw6 ph0 mh0'>
-                               Register
-                            </legend>
-                            <div className='mt3'>
-                                <label 
-                                    className='db fw6 lh-copy f6' 
-                                    htmlFor='name'
-                                >
-                                    Name
-                                </label>
-                                <input 
-                                    className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100' 
-                                    type='text' 
-                                    name='name'  
-                                    id='name' 
-                                    onChange={this.onNameChange}
-                                />
+            <div className='register-container'>
+                {this.state.isLoading
+                    ? <PacmanLoader className='loading-spinner' color="#66F1FF" />
+                    : <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
+                        <main className='pa4 black-80 w-100'>
+                            <div className='measure'>
+                                <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+                                    <legend className='f2 fw6 ph0 mh0'>
+                                        Register
+                                    </legend>
+                                    <div className='mt3'>
+                                        <label
+                                            className='db fw6 lh-copy f6'
+                                            htmlFor='name'
+                                        >
+                                            Name
+                                        </label>
+                                        <input
+                                            className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                                            type='text'
+                                            name='name'
+                                            id='name'
+                                            onChange={this.onNameChange}
+                                        />
+                                    </div>
+                                    <div className='mt3'>
+                                        <label
+                                            className='db fw6 lh-copy f6'
+                                            htmlFor='email-address'
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                                            type='email'
+                                            name='email-address'
+                                            id='email-address'
+                                            onChange={this.onEmailChange}
+                                            onKeyDown={this.handleKeyPress}
+                                        />
+                                    </div>
+                                    <div className='mv3'>
+                                        <label
+                                            className='db fw6 lh-copy f6'
+                                            htmlFor='password'
+                                        >
+                                            Password
+                                        </label>
+                                        <input
+                                            className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                                            type='password'
+                                            name='password'
+                                            id='password'
+                                            onChange={this.onPasswordChange}
+                                            onKeyDown={this.handleKeyPress}
+                                        />
+                                    </div>
+                                </fieldset>
+                                <div>
+                                    <input
+                                        className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w-100'
+                                        type='submit'
+                                        value='Register'
+                                        onClick={this.onSubmit}
+                                    />
+                                </div>
                             </div>
-                            <div className='mt3'>
-                                <label 
-                                    className='db fw6 lh-copy f6' 
-                                    htmlFor='email-address'
-                                >
-                                    Email
-                                </label>
-                                <input 
-                                    className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100' 
-                                    type='email' 
-                                    name='email-address'  
-                                    id='email-address' 
-                                    onChange={this.onEmailChange}
-                                    onKeyDown={this.handleKeyPress}
-                                />
-                            </div>
-                            <div className='mv3'>
-                                <label 
-                                    className='db fw6 lh-copy f6' 
-                                    htmlFor='password'
-                                >
-                                    Password
-                                </label>
-                                <input 
-                                    className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100' 
-                                    type='password' 
-                                    name='password'  
-                                    id='password' 
-                                    onChange={this.onPasswordChange}
-                                    onKeyDown={this.handleKeyPress}
-                                />
-                            </div>
-                        </fieldset>
-                        <div>
-                            <input 
-                                className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w-100' 
-                                type='submit' 
-                                value='Register'
-                                onClick={this.onSubmit}
-                            />
-                        </div>
-                    </div>
-                </main>
-            </article>
+                        </main>
+                    </article>
+                }
+            </div>
         );
     }
 };
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { PacmanLoader } from 'react-spinners';
+
+import './SigIn.css';
 
 class SignIn extends Component {
     constructor(props) {
         super(props);
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            isLoading: false
         }
     }
 
@@ -18,9 +22,10 @@ class SignIn extends Component {
     }
 
     onSubmit = () => {
+        this.setState({ isLoading: true })
         fetch('https://face-recognition-brain-api-gu0q.onrender.com/signin', {
             method: 'post',
-            headers: {'Content-type': 'application/json'},
+            headers: { 'Content-type': 'application/json' },
             body: JSON.stringify({
                 email: this.state.email,
                 password: this.state.password
@@ -29,6 +34,7 @@ class SignIn extends Component {
             .then(response => response.json())
             .then(user => {
                 user.id && this.props.onRouteChange('home')
+                this.setState({ isLoading: false })
             })
             .catch(console.log)
     }
@@ -43,66 +49,71 @@ class SignIn extends Component {
 
     render() {
         const { onRouteChange } = this.props;
-        return(
-            <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
-                <main className='pa4 black-80 w-100'>
-                    <div className='measure'>
-                        <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
-                            <legend className='f2 fw6 ph0 mh0'>
-                                Sign In
-                            </legend>
-                            <div className='mt3'>
-                                <label 
-                                    className='db fw6 lh-copy f6' 
-                                    htmlFor='email-address'
-                                >
-                                    Email
-                                </label>
-                                <input 
-                                    className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100' 
-                                    type='email' 
-                                    name='email-address'  
-                                    id='email-address' 
-                                    onChange={this.onEmailChange}
-                                    onKeyDown={this.handleKeyPress}
-                                />
+        return (
+            <div className='sign-in-container'>
+                {this.state.isLoading
+                    ? <PacmanLoader className='loading-spinner' color="#66F1FF" />
+                    : <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center'>
+                        <main className='pa4 black-80 w-100'>
+                            <div className='measure'>
+                                <fieldset id='sign_up' className='ba b--transparent ph0 mh0'>
+                                    <legend className='f2 fw6 ph0 mh0'>
+                                        Sign In
+                                    </legend>
+                                    <div className='mt3'>
+                                        <label
+                                            className='db fw6 lh-copy f6'
+                                            htmlFor='email-address'
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            className='pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                                            type='email'
+                                            name='email-address'
+                                            id='email-address'
+                                            onChange={this.onEmailChange}
+                                            onKeyDown={this.handleKeyPress}
+                                        />
+                                    </div>
+                                    <div className='mv3'>
+                                        <label
+                                            className='db fw6 lh-copy f6'
+                                            htmlFor='password'
+                                        >
+                                            Password
+                                        </label>
+                                        <input
+                                            className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100'
+                                            type='password'
+                                            name='password'
+                                            id='password'
+                                            onChange={this.onPasswordChange}
+                                            onKeyDown={this.handleKeyPress}
+                                        />
+                                    </div>
+                                </fieldset>
+                                <div>
+                                    <input
+                                        className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w-100'
+                                        type='submit'
+                                        value='Sign in'
+                                        onClick={this.onSubmit}
+                                    />
+                                </div>
+                                <div className='lh-copy mt3'>
+                                    <span
+                                        className='f6 link dim black db pointer'
+                                        onClick={() => onRouteChange('register')}
+                                    >
+                                        Register
+                                    </span>
+                                </div>
                             </div>
-                            <div className='mv3'>
-                                <label 
-                                    className='db fw6 lh-copy f6' 
-                                    htmlFor='password'
-                                >
-                                    Password
-                                </label>
-                                <input 
-                                    className='b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100' 
-                                    type='password' 
-                                    name='password'  
-                                    id='password' 
-                                    onChange={this.onPasswordChange}
-                                    onKeyDown={this.handleKeyPress}
-                                />
-                            </div>
-                        </fieldset>
-                        <div>
-                            <input 
-                                className='b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib w-100' 
-                                type='submit' 
-                                value='Sign in'
-                                onClick={this.onSubmit}
-                            />
-                        </div>
-                        <div className='lh-copy mt3'>
-                            <span 
-                                className='f6 link dim black db pointer'
-                                onClick={() => onRouteChange('register')}
-                            >
-                                Register
-                            </span>
-                        </div>
-                    </div>
-                </main>
-            </article>
+                        </main>
+                    </article>
+                }
+            </div>
         );
     }
 };
